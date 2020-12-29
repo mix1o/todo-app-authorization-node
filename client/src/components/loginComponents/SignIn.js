43afe0053d8d76f7dc2d0cc7494436faf3ab22fb
nightmare9254 <adrian.sarna59@gmail.com>
2020-12-29 19:41:46 +0100

    history push

--- client/src/components/loginComponents/SignIn.js
+++ client/src/components/loginComponents/SignIn.js
@@ -30,7 +30,7 @@ const SignIn = () => {
       .then((json) => {
         setMessage(json);
         if (json.correct) {
-          window.location.href = '/user-panel';
+          history.push('/user-panel');
         }
       });
   };
