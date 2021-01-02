fabbeed14a3cbb25393aef2f1031884cdadcbc29
nightmare9254 <adrian.sarna59@gmail.com>
2021-01-02 12:02:07 +0100

    asd

--- index.js
+++ index.js
@@ -43,10 +43,10 @@ app.get('*', (req, res) => {
 
   if (protected.includes(path)) {
     // Return the actual file
-    res.sendFile(`./client/build/${path}`);
+    res.sendFile(`${__dirname}/client/build/${path}`);
   } else {
     // Otherwise, redirect to /build/index.html
-    res.sendFile(`./client/build/index.html`);
+    res.sendFile(`${__dirname}/client/build/index.html`);
   }
 });
 
