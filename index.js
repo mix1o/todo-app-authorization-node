124b722bc0b188db1e3862c4b55c2f1288a46ebd
nightmare9254 <adrian.sarna59@gmail.com>
2021-01-03 10:17:25 +0100

    asd

--- index.js
+++ index.js
@@ -30,16 +30,8 @@ mongoose
 
 const userRoutes = require('./routes/userRoutes');
 
-let protected = [
-  'transformed.js',
-  'svg.js',
-  'MainStyle.css',
-  'main.css',
-  'favicon.ico',
-];
-
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static('/client/build'));
   app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, './client/build/index.html'));
   });
