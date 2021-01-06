42fbaab797277763cb57349bbe6efcd8efb453ba
nightmare9254 <adrian.sarna59@gmail.com>
2021-01-06 11:20:43 +0100

    wyjebalem ifa

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const userRoutes = require('./routes/userRoutes');
 
 app.use(express.static('client/build'));
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'client/build/index.html'));
+  res.sendFile(path.join(__dirname, 'build/index.html'));
 });
 
 app.use(cookieParser());
