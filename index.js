a9749406eb5820e4ebd2871eb62ce04a2d602e8f
nightmare9254 <adrian.sarna59@gmail.com>
2021-01-06 10:39:52 +0100

    wyjebalem ifa

--- index.js
+++ index.js
@@ -31,9 +31,9 @@ mongoose
 const userRoutes = require('./routes/userRoutes');
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('/client/build'));
+  app.use(express.static('./client/build'));
   app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, '/client/build/index.html'));
+    res.sendFile(path.join(__dirname, './client/build/index.html'));
   });
 }
 
